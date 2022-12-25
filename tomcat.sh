5369cd07f613022765ddff536416512d1556d46a
Anhgrew <socksockmoizui@gmail.com>
2022-12-26 00:01:32 +0700

    feat: Update pom.xml

--- tomcat.sh
+++ tomcat.sh
@@ -19,25 +19,15 @@ Description=Tomcat
 After=network.target
 
 [Service]
-
 User=tomcat
-Group=tomcat
-
 WorkingDirectory=/usr/local/tomcat8
-
-#Environment=JRE_HOME=/usr/lib/jvm/jre
+Environment=JRE_HOME=/usr/lib/jvm/jre
 Environment=JAVA_HOME=/usr/lib/jvm/jre
-
-Environment=CATALINA_PID=/var/tomcat/%i/run/tomcat.pid
 Environment=CATALINA_HOME=/usr/local/tomcat8
 Environment=CATALINE_BASE=/usr/local/tomcat8
-
 ExecStart=/usr/local/tomcat8/bin/catalina.sh run
 ExecStop=/usr/local/tomcat8/bin/shutdown.sh
-
-
-RestartSec=10
-Restart=always
+SyslogIdentifier=tomcat-%i
 
 [Install]
 WantedBy=multi-user.target
